d2cb2d67553200834e68aaef4cdfedf82920b001
KarolisKaraliunas <kkaraliunas1@gmail.com>
2021-02-18 13:42:24 +0000

    Add New Files

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "t4RG*8xLf31Vl%eg%Ysq4O,%>DfOE@")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://<USERNAME>:<PASSWORD>@<CLUSTER>-4g3i1.mongodb.net/<DATABASE>?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@<CLUSTER>-4g3i1.mongodb.net/Cluster0?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "task_manager")
