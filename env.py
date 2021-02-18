1a54cbc8529f28cdc3946ab169e0e2b3ed73dcf1
KarolisKaraliunas <kkaraliunas1@gmail.com>
2021-02-18 14:33:03 +0000

    Create base template

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "t4RG*8xLf31Vl%eg%Ysq4O,%>DfOE@")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@<CLUSTER>-4g3i1.mongodb.net/Cluster0?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@cluster0.ll1vp.mongodb.net/task_manager?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "task_manager")
